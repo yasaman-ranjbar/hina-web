63a1d791bb75cdddf3aff93dac73545e78520821
unknown <y.ranjbar6698@gmail.com>
2024-04-14 09:36:31 +0330

    add testimonial

---
+++ src/app/[lng]/_components/testimonial/testimonial.types.ts
@@ -0,0 +1,9 @@
+import { Testimonial } from "@/types/testimonials.type"
+
+export type TestimonialProps = {
+    testimonial: Testimonial
+}
+
+export type TestimonialListProps = {
+    testimonials: Testimonial[]
+}
--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -11,6 +11,8 @@ import { BlogPostCardList } from "./(blog)/_components/blog-post-card-list";
 import { API_URL } from "@/configs/global";
 import { Suspense } from "react";
 import { CardPlaceholder } from "./_components/placeholders";
+import { TestimonialList } from "./_components/testimonial/testimonial-list";
+import { testimonials } from "@/data/testimonials";
 
 type paramsProps = {
   params: {
@@ -128,6 +130,19 @@ export default async function Home({ params: { lng } }: paramsProps) {
         </div>
         <BlogPostCardList posts={newestBlogPosts} />
       </section>
+
+      <div className="relative mt-32">
+        <div className="bg-primary pointer-events-none absolute bottom-0 left-1/2 aspect-square w-1/2 -translate-x-1/2 rounded-full opacity-5 -top-52 blur-3xl"></div>
+        <h2 className="text-info relative z-0 mx-auto text-3xl font-extrabold block w-fit">
+          <span className="-z-10 w-8 h-8 absolute bg-info opacity-25 -top-2 rounded-full inline-block -right-3"></span>
+          تجربه هم‌میسرهای کلاسبن
+        </h2>
+        <p className=" mb-32 text-lg text-center mt-2">
+          تو اینجا تنها نیستی. ببین هم‌مسیرهات نظرشون در مورد دوره‌های کلاسبن
+          چیه
+        </p>
+        <TestimonialList testimonials={testimonials} />
+      </div>
     </>
   );
 }
