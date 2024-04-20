f91dc4dc34de32483947f2a4383be567ace0750f
unknown <y.ranjbar6698@gmail.com>
2024-04-20 16:30:19 +0330

    add infinit scroll

--- src/app/[lng]/(courses)/courses/[slug]/_api/get-comments.ts
+++ src/app/[lng]/(courses)/courses/[slug]/_api/get-comments.ts
@@ -1,6 +1,6 @@
 import { readData } from "@/core/http-service/http-service";
 import { commentListProps } from "../_types/course-comment.interface";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 type commentParamsProps = {
     params: {
@@ -17,12 +17,14 @@ const getComments = ({ params }: commentParamsProps): Promise<commentListProps> 
 
 export const useCourseComments = ({ params }: commentParamsProps) => {
 
-    const { data , isLoading } = useQuery({
-        queryKey: ['courseComments'],
-        queryFn: () => getComments({ params }),
+    const { data, isFetching , fetchNextPage, isFetchingNextPage, hasNextPage, refetch, error } = useInfiniteQuery({
+        queryKey: ['courseComments' , params.slug],
+        queryFn: ({ pageParam  }) => getComments({ params: { ...params, page: pageParam } }),
+        getNextPageParam: (lastPage) => lastPage.nextPage,
+        initialPageParam: 1,
         staleTime: 5 * 60 * 60 * 1000,
         gcTime: 6 * 60 * 60 * 1000, // 6 hours
     })
 
-    return { data, isLoading };
+    return { data, isFetching, fetchNextPage, isFetchingNextPage, hasNextPage, refetch, error };
 }
--- src/app/[lng]/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/[lng]/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -4,24 +4,55 @@ import { useParams } from "next/navigation";
 import { useCourseComments } from "../../_api/get-comments";
 import { Comment } from "@/app/[lng]/_components/comments/comments";
 import { TextPlaceholder } from "@/app/[lng]/_components/placeholders";
+import { Fragment, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 function CourseComments() {
+  const { ref, inView } = useInView({});
   const { slug } = useParams();
 
-  const { data: comments, isLoading } = useCourseComments({
+  const {
+    data: comments,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    refetch,
+  } = useCourseComments({
     params: {
       slug: slug as string,
       page: 1,
     },
   });
 
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
+  
+
   return (
     <div>
-      {comments?.data.map((comment) => (
-        <Comment key={`comment-${comment.id}`} {...comment} variant="info" />
+      {comments?.pages.map((item) => (
+        <Fragment key={`comment-page-${item}`}>
+          {item.data.map((comment) => (
+            <Comment
+              key={`comment-${comment.id}`}
+              {...comment}
+              variant="info"
+            />
+          ))}
+        </Fragment>
       ))}
 
-      {isLoading && <TextPlaceholder />}
+      {isFetchingNextPage ||
+        (hasNextPage && (
+          <div ref={ref}>
+            <TextPlaceholder />
+          </div>
+        ))}
     </div>
   );
 }
